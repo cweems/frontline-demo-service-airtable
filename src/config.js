b9ae69af65d8f2b995201d743e334f2c3b214370
Charlie Weems <charlie.weems@gmail.com>
2021-07-23 18:15:28 -0700

    Add airtable integration with memoization

--- src/config.js
+++ src/config.js
@@ -11,6 +11,9 @@ module.exports = {
         auth_token: _('TWILIO_AUTH_TOKEN'),
         sso_realm_sid: _('TWILIO_SSO_REALM_SID'),
         sms_number: _('TWILIO_SMS_NUMBER'),
-        whatsapp_number: _('TWILIO_WHATSAPP_NUMBER')
+        whatsapp_number: _('TWILIO_WHATSAPP_NUMBER'),
+        airtable_api_key: _('AIRTABLE_API_KEY'),
+        airtable_base_id: _('AIRTABLE_BASE_ID'),
+        airtable_table_name: _('AIRTABLE_TABLE_NAME')
     },
 };
--- src/providers/customers.js
+++ src/providers/customers.js
@@ -9,31 +9,62 @@
 //     {
 //         'whatsapp:+12345678': 'john@example.com'
 //     }
+
+const config = require('../config');
 const customersToWorkersMap = {};
 
-// Customers list
-// Example:
-// [
-//   {
-//      customer_id: 98,
-//      display_name: 'Bobby Shaftoe',
-//      channels: [
-//          { type: 'email', value: 'bobby@example.com' },
-//          { type: 'sms', value: '+123456789' },
-//          { type: 'whatsapp', value: 'whatsapp:+123456789' }
-//      ],
-//      links: [
-//          { type: 'Facebook', value: 'https://facebook.com', display_name: 'Social Media Profile' }
-//      ],
-//      details:{
-//          title: "Information",
-//          content: "Status: Active" + "\n\n" + "Score: 100"
-//      },
-//      worker: 'john@example.com'
-//   }
-// ]
+// Create global variable to memoize customer data
+// so that we do not ping airtable for all customers every page load
+let customers = [];
 
-const customers = [];
+// Retrieve customers from Airtable
+// Example:
+const retrieveAirtableData = async () => {
+    var Airtable = require('airtable');
+    var base = new Airtable({apiKey: config.twilio.airtable_api_key}).base(config.twilio.airtable_base_id);
+
+    return new Promise((resolve, reject) => {
+        let formattedCustomers = [];
+    
+        base(config.twilio.airtable_table_name).select({
+            view: "Grid view",
+            pageSize: 100
+        }).eachPage(function page(records, fetchNextPage) {
+            // This function (`page`) will get called for each page of records.
+        
+            records.forEach(function(record) {
+                let formattedRecord = {
+                    customer_id: record.get('id'),
+                    display_name: record.get('name'),
+                    channels: [
+                        { type: 'sms', value: record.get('sms') },
+                        { type: 'whatsapp', value: record.get('whatsapp') }
+                    ],
+                    links: [
+                        { type: 'LinkedIn', value: record.get('linkedin'), display_name: 'Social Media Profile' }
+                    ],
+                    details:{
+                        title: "Information",
+                        content: record.get('notes')
+                    },
+                    worker: record.get('owner')
+                }
+                formattedCustomers.push(formattedRecord);
+            });
+        
+            // To fetch the next page of records, call `fetchNextPage`.
+            // If there are more records, `page` will get called again.
+            // If there are no more records, `done` will get called.
+            console.log('fetching next page...')
+            fetchNextPage();
+        
+        }, function done(err) {
+            if (err) { reject(err) }
+            resolve(formattedCustomers);
+        });
+    });
+
+}
 
 const findWorkerForCustomer = async (customerNumber) => customersToWorkersMap[customerNumber];
 
@@ -49,6 +80,13 @@ const findRandomWorker = async () => {
 }
 
 const getCustomersList = async (worker, pageSize, anchor) => {
+    console.log(pageSize, anchor);
+
+    // Pull airtable customers on first load, otherwise use
+    // what's stored in memory
+    if(anchor === undefined || customers.length === 0) {
+        customers = await retrieveAirtableData();
+    }
     const workerCustomers = customers.filter(customer => customer.worker === worker);
     const list = workerCustomers.map(customer => ({
         display_name: customer.display_name,
@@ -70,10 +108,16 @@ const getCustomersList = async (worker, pageSize, anchor) => {
 };
 
 const getCustomerByNumber = async (customerNumber) => {
+    if (customers.length === 0) {
+        customers = await retrieveAirtableData();
+    }
     return customers.find(customer => customer.channels.find(channel => String(channel.value) === String(customerNumber)));
 };
 
 const getCustomerById = async (customerId) => {
+    if (customers.length === 0) {
+        customers = await retrieveAirtableData();
+    }
     return customers.find(customer => String(customer.customer_id) === String(customerId));
 };
 
